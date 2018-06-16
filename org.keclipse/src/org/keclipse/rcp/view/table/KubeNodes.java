a638b57feebbc09bd59a6888215e8a689dc5cc94
vibhu.pratap <vibhu.pratap@veritas.com>
2018-06-16 09:49:27 -0700

    Added support for Pods

--- org.keclipse/src/org/keclipse/rcp/view/table/KubeNodes.java
+++ org.keclipse/src/org/keclipse/rcp/view/table/KubeNodes.java
@@ -9,7 +9,6 @@ import io.kubernetes.client.models.V1Node;
 
 /**
  * Get the nodes associated with the KubeConfig
- * 
  * @author vibhu.pratap
  *
  */
@@ -17,29 +16,28 @@ public class KubeNodes extends AbstractTableView {
 
 	@Override
 	public Map<Integer, String> getProperties(Object element) {
-		Map<Integer, String> mmap = new HashMap<Integer, String>();
+		Map<Integer,String> mmap = new HashMap<Integer,String>();
 		V1Node pod = (V1Node) element;
 		mmap.put(0, pod.getMetadata().getName());
 		mmap.put(1, pod.getMetadata().getCreationTimestamp().toString());
 		mmap.put(2, pod.getSpec().getPodCIDR());
 		return mmap;
 	}
-
+	
 	/**
 	 * Only these column names needed
 	 */
 	@Override
 	public String[] getTitles() {
-		String[] titles = { "Name", "CreationTime", "PodCidr" };
+		String[] titles = { "Name","CreationTime","PodCidr"};
 		return titles;
 	}
-
+	
 	@Override
 	public String[] getWideTitles() {
-		String[] wide = { "Name", "CreationTime" };
+		String[] wide = { "Name" ,"CreationTime"};
 		return wide;
 	}
-
 	@Override
 	public String getObjectName() {
 		return IKubeEclipseConstants.IKubeObjects.NODE;
---
+++ org.keclipse/src/org/keclipse/rcp/view/table/KubePods.java
@@ -0,0 +1,47 @@
+package org.keclipse.rcp.view.table;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.keclipse.utils.IKubeEclipseConstants;
+
+import io.kubernetes.client.models.V1Pod;
+
+/**
+ * View for Kubernetes Pods
+ * 
+ * @author vibhu.pratap
+ *
+ */
+public class KubePods extends AbstractTableView {
+
+	@Override
+	public Map<Integer, String> getProperties(Object element) {
+		Map<Integer, String> mmap = new HashMap<Integer, String>();
+		V1Pod pod = (V1Pod) element;
+		mmap.put(0, pod.getMetadata().getName());
+		mmap.put(1, pod.getStatus().getPodIP());
+		mmap.put(2, pod.getStatus().getPhase());
+		mmap.put(3, pod.getMetadata().getNamespace());
+		mmap.put(4, pod.getSpec().getNodeName());
+		return mmap;
+	}
+
+	@Override
+	public String[] getTitles() {
+		String[] titles = { "Name", "IP", "Phase", "Namespace", "NodeName" };
+		return titles;
+	}
+
+	@Override
+	public String[] getWideTitles() {
+		String[] wide = { "Name", "NodeName" };
+		return wide;
+	}
+
+	@Override
+	public String getObjectName() {
+		return IKubeEclipseConstants.IKubeObjects.POD;
+	}
+
+}
